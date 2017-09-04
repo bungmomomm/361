eb035c5fb40d14bf74d15881193449b57041d0c0
sarwonous <agus.sarwono@gmail.com>
2017-09-04 14:29:09 +0700

    user baru yan-g belum punya billing history , pada saat add new alamat nomor telphon tidak reflec dengan  billing yang ada di payment (no yg tlp diatas no ovo)

--- src/containers/Checkout/index.js
+++ src/containers/Checkout/index.js
@@ -762,11 +762,9 @@ class Checkout extends Component {
 		dispatch(saveAddress(this.props.cookies.get('user.token'), formData));
 		if (typeof this.props.billing[0] === 'undefined') {
 			dispatch(changeBillingNumber(formData.no_hp));
-		} else {
-			if (!this.props.payments.billingPhoneNumberEdited) {
-				const billing = this.props.billing[0];
-				dispatch(changeBillingNumber(billing.attributes.phone));
-			}
+		} else if (!this.props.payments.billingPhoneNumberEdited) {
+			const billing = this.props.billing[0];
+			dispatch(changeBillingNumber(billing.attributes.phone));
 		}
 		this.setState({
 			enableNewAddress: false,
