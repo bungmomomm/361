e92451ae36545a5d3b833e4e04e46f02d04d8bb9
vinensiuswibowo <bowo@mataharimall.com>
2017-11-07 10:59:40 +0700

    remove unused import Proptypes

--- src/components/Modules/Select/Select.js
+++ src/components/Modules/Select/Select.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { injectProps } from '@/decorators';
 import styles from './Select.scss';
 import classNames from 'classnames/bind';
