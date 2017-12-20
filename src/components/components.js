68005b9ff352a296e3c236870f861526cfc76bcb
vinensiuswibowo <bowo@mataharimall.com>
2017-12-20 16:17:49 +0700

    remove icon component

--- src/components/components.js
+++ src/components/components.js
@@ -3,13 +3,11 @@ import CreditCardInput from './CreditCardInput/CreditCardInput';
 import CreditCardRadio from './CreditCardRadio/CreditCardRadio';
 import Sprites from './Sprites/Sprites';
 import Image from './Image/Image';
-import Icon from './Icon/Icon';
 
 export default {
 	CheckoutHeader,
 	CreditCardInput,
 	CreditCardRadio,
 	Sprites,
-	Image,
-	Icon
+	Image
 };
