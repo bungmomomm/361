f0c1da26e33dc362d14891742a72534979c4ed2d
silvia <silvi.fang@gmail.com>
2018-01-10 10:59:12 +0700

    change app_env to node_env

--- src/state/Auth/actions.js
+++ src/state/Auth/actions.js
@@ -38,7 +38,7 @@ const getRefreshToken = (token) => dispatch => new Promise((resolve, reject) => 
 	})
 	.catch((error) => {
 		// redirect back to main or home page if an user-customer has not logged in yet
-		const isProduction = process.env.APP_ENV.toUpperCase() === 'PRODUCTION';
+		const isProduction = process.env.NODE_ENV.toUpperCase() === 'PRODUCTION';
 		if ((error.response.data.code === 401 || error.response.data.code === 403) && isProduction) {
 			top.location.href = getBaseUrl();
 		}
