2df8dd036dbee38ef5ee9611db6bbe7d2c5fc1d3
ijalandhika <ijalandhika@gmail.com>
2018-01-19 14:22:05 +0700

    react router with containers

--- src/containers/Discovery/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default (props) => {
-	return (
-		<main>
-			<h1>Ngapain hayo </h1>
-		</main>
-	);
-};
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,22 +2,26 @@
 import loadable from 'loadable-components';
 import { isMobile } from '@/utils';
 
-const Page = loadable(() => import('@/containers/Page'));
-const Home = isMobile() ? 
-			loadable(() => import('@/containers/Discovery/Home/Mobile')) 
-			: loadable(() => import('@/containers/Discovery/Home/Desktop'));
-// const Test = loadable(() => import('@/containers/Discovery/Desktop'));
+
+
+// const Page = loadable(() => import('@/containers/Page'));
+
+/**
+ * cannot using like this 
+ * const type = isMobile() ? 'Mobile' : 'Desktop';
+ * const Home = loadable(() => import(`@/containers/${type}/Home`));
+ * since genereated js file will mix dekstop & mobile
+ */
+
+const Home = isMobile ? loadable(() => import('@/containers/Mobile/Home')) 
+	: loadable(() => import('@/containers/Desktop/Home'));
 
 export default [
 
 	{
 		path: '/',
-		component: Page,
-		exact: true
-	},
-	{
-		path: '/home',
 		component: Home,
+		exact: true
 	}
-	
+
 ];
