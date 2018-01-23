f866ac24a2fe26711f0a446ad1ae590266142bfc
sarwonous <agus.sarwono@gmail.com>
2018-01-23 16:13:31 +0700

    add reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,21 +1,21 @@
 import { combineReducers } from 'redux';
-// import { reducer as tracking } from '@/state/Tracking/';
-// import { reducer as coupon } from '@/state/Coupon/';
-// import { reducer as addresses } from '@/state/Adresses/';
-// import { reducer as api } from '@/state/Api/';
-// import { reducer as cart } from '@/state/Cart/';
+import { reducer as tracking } from '@/state/Tracking/';
+import { reducer as coupon } from '@/state/Coupon/';
+import { reducer as addresses } from '@/state/Adresses/';
+import { reducer as api } from '@/state/Api/';
+import { reducer as cart } from '@/state/Cart/';
 import { reducer as payments } from '@/state/Payment/';
 import { reducer as user } from '@/state/User/';
 import { reducer as global } from '@/state/Global';
 import { reducer as users } from '@/state/v4/User';
 
 export default combineReducers({
-	// ...global,
-	// tracking,
-	// coupon,
-	// addresses,
-	// api,
-	// cart,
+	...global,
+	tracking,
+	coupon,
+	addresses,
+	api,
+	cart,
 	payments,
 	user,
 	global,
