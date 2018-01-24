5f179018ecce552bd9c7ba1c230861f702e2ab1b
vinensiuswibowo <bowo@mataharimall.com>
2018-01-24 14:02:28 +0700

    resolve conflict

--- src/containers/Mobile/Discovery/Lovelist/index.js
+++ src/containers/Mobile/Discovery/Lovelist/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withCookies } from 'react-cookie';
-import { Header, Page, Card, Button, Svg, Image } from '@/components/mobile';
+import { Header, Page, Card, Button, Svg, Image, Level } from '@/components/mobile';
 import styles from './lovelist.scss';
 import * as data from '@/data/example/Lovelist';
 
@@ -9,7 +9,9 @@ class Lovelist extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			listTypeGrid: false
+			listTypeGrid: false,
+			listEmpty: false,
+			loggedIn: false
 		};
 	}
 
@@ -17,7 +19,7 @@ class Lovelist extends Component {
 		const { listTypeGrid } = this.state;
 		const HeaderPage = {
 			left: (
-				<Button onClick={() => this.setState({ listTypeGrid: !listTypeGrid })}>
+				<Button className={this.state.loggedIn && !this.state.listEmpty ? null : 'd-none'} onClick={() => this.setState({ listTypeGrid: !listTypeGrid })}>
 					<Svg src={!listTypeGrid ? 'ico_grid.svg' : 'ico_list.svg'} />
 				</Button>
 			), 
@@ -29,32 +31,56 @@ class Lovelist extends Component {
 			)
 		};
 
-		return (
-			<div style={this.props.style}>
-				<Header.Lovelist listTypeGrid={this.state.listTypeGrid} toggleGrid={() => this.setState({ listTypeGrid: !this.state.listTypeGrid })} />
-				<Page>
-					{ data.Lovelist.products.length > 0 ? (
-						<div className='text-center --disable-flex'>
-							<p className='margin--medium'>Kamu belum memiliki Lovelist</p>
-							<p className='margin--medium font--lato-light'>Tap the <Svg src='ico_love.svg' /> next to an item to add
-								<br />it to your Lovelist.
-							</p>
-							<p className='margin--medium'><Button inline size='large' color='primary'>BELANJA</Button></p>
-							<Image local style={{ marginBottom: '-30px' }} alt='Tap the love icon' src='lovelist-guide.png' />
-						</div>
-					) : (
-						<div className={styles.cardContainer}>
-							{!listTypeGrid ? <Card.Lovelist /> : <Card.LovelistGrid />}
-							{!listTypeGrid ? <Card.Lovelist /> : <Card.LovelistGrid />}
-							{!listTypeGrid ? <Card.Lovelist /> : <Card.LovelistGrid />}
-							{!listTypeGrid ? <Card.Lovelist /> : <Card.LovelistGrid />}
-						</div>
-						)
-					}
-				</Page>
-				<Header.Modal {...HeaderPage} />
+		const renderLovelistPage = (content) => {
+			return (
+				<div style={this.props.style}>
+					<Header.Lovelist 
+						listTypeGrid={this.state.listTypeGrid} 
+						toggleGrid={() => this.setState({ listTypeGrid: !this.state.listTypeGrid })} 
+					/>
+					<Page>
+						{content}
+					</Page>
+					<Header.Modal {...HeaderPage} />
+				</div>
+			);
+		};
+		if (!this.state.loggedIn) {
+			return (renderLovelistPage(
+				<div style={{ marginTop: '60px', padding: '20px' }} className='text-center --disable-flex'>
+					<Svg src='ico_ghost.svg' />
+					<p className='margin--medium'>Unlock the Full Experience</p>
+					<Level className='margin--medium'>
+						<Level.Left>&nbsp;</Level.Left>
+						<Level.Item>
+							<p className='margin--small'><Button wide size='large' color='primary'>LOGIN</Button></p>
+							<p className='margin--small'><Button wide outline size='large' color='primary'>DAFTAR</Button></p>
+						</Level.Item>
+						<Level.Right>&nbsp;</Level.Right>
+					</Level>
+				</div>
+			));
+		}
+		if (this.state.listEmpty) {
+			return (renderLovelistPage(
+				<div className='text-center --disable-flex'>
+					<p className='margin--medium'>Kamu belum memiliki Lovelist</p>
+					<p className='margin--medium font--lato-light'>Tap the <Svg src='ico_love.svg' /> next to an item to add
+						<br />it to your Lovelist.
+					</p>
+					<p className='margin--medium'><Button inline size='large' color='primary'>BELANJA</Button></p>
+					<Image local style={{ marginBottom: '-30px' }} alt='Tap the love icon' src='lovelist-guide.png' />
+				</div>
+			));
+		}
+		return (renderLovelistPage(
+			<div className={styles.cardContainer}>
+				{!listTypeGrid ? <Card.Lovelist /> : <Card.LovelistGrid />}
+				{!listTypeGrid ? <Card.Lovelist /> : <Card.LovelistGrid />}
+				{!listTypeGrid ? <Card.Lovelist /> : <Card.LovelistGrid />}
+				{!listTypeGrid ? <Card.Lovelist /> : <Card.LovelistGrid />}
 			</div>
-		);
+		));
 	}
 }
 
