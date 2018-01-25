dd59fa347bfe0aaa599237006502ff2dca30b7e5
sarwonous <agus.sarwono@gmail.com>
2018-01-25 10:36:06 +0700

    add otp

--- src/state/v4/User/reducer.js
+++ src/state/v4/User/reducer.js
@@ -1,12 +1,30 @@
 import { handleActions, createActions } from 'redux-actions';
 
 const actions = createActions({
-	LOGIN: (email, password) => ({ email, password }),
-	USER_LOGIN_FAIL: () => ({ password: '' }),
-	USER_LOGIN_SUCCESS: (userprofile) => ({ email: undefined, password: undefined, userprofile }),
+	USER_LOGIN: undefined,
+	USER_LOGIN_FAIL: (error) => ({ login: { error } }),
+	USER_LOGIN_SUCCESS: (userProfile) => ({ email: undefined, password: undefined, userProfile }),
 	USER_ANONYMOUS: undefined,
-	USER_LOGIN_ANONYMOUS_SUCCESS: (userprofile) => ({ userprofile }),
-	USER_NAME_CHANGE: (username) => ({ username })
+	USER_LOGIN_ANONYMOUS_SUCCESS: (userProfile) => ({ userProfile }),
+	USER_NAME_CHANGE: (email) => ({ email }),
+	USER_REGISTER: undefined,
+	USER_REGISTER_FAIL: (error) => ({ register: { error } }),
+	USER_REGISTER_SUCCESS: (userProfile) => ({ userProfile }),
+	USER_OTP: undefined,
+	USER_OTP_SUCCESS: (message) => ({ otp: { message } }),
+	USER_OTP_FAIL: (error) => ({ otp: { error } }),
+	USER_OTP_VALIDATE: undefined,
+	USER_OTP_VALIDATE_SUCCESS: (userProfile) => ({ userProfile }),
+	USER_OTP_VALIDATE_FAIL: (error) => ({ otp: { error } }),
+	USER_GET_PROFILE: undefined,
+	USER_GET_PROFILE_FAIL: (error) => ({ profile: { error } }),
+	USER_GET_PROFILE_SUCCESS: (userProfile) => ({ userProfile }),
+	USER_FORGET_PASSWORD: undefined,
+	USER_FORGET_PASSWORD_FAIL: (error) => ({ forgot: { error } }),
+	USER_FORGET_PASSOWRD_SUCCESS: (message) => ({ forget: { message } }),
+	USER_NEW_PASSWORD: undefined,
+	USER_NEW_PASSWORD_FAIL: (error) => ({ newpassword: { error } }),
+	USER_NEW_PASSWORD_SUCCESS: (message) => ({ newpassword: { message } })
 });
 
 const initialState = {
@@ -17,39 +35,21 @@ const initialState = {
 };
 
 const reducer = handleActions({
-	[actions.login](state, action) {
-		return {
-			...state,
-			...action.payload,
-			isLoading: true
-		};
-	},
+	[actions.userLogin]: (state, action) => ({ ...state, ...action.payload, isLoading: true }),
 	[actions.userLoginSuccess](state, action) {
 		return {
 			...state,
-			userprofile: action.payload.userprofile.data.info,
+			userprofile: action.payload.userProfile.data.info,
 			isLoading: false,
 			isAnonymous: false
 		};
 	},
-	[actions.userLoginFail](state, action) {
-		return {
-			...state,
-			...action.payload,
-			isLoading: false
-		};
-	},
-	[actions.userAnonymous](state, action) {
-		return {
-			...state,
-			...action.payload,
-			isLoading: true 
-		};
-	},
+	[actions.userLoginFail]: (state, action) => ({ ...state, ...action.payload, isLoading: false }),
+	[actions.userAnonymous]: (state, action) => ({ ...state, ...action.payload, isLoading: true }),
 	[actions.userLoginAnonymousSuccess](state, action) {
 		return {
 			...state,
-			userprofile: action.payload.userprofile.data.info,
+			userprofile: action.payload.userProfile.data.info,
 			isLoading: false,
 			isAnonymous: true
 		};
@@ -60,6 +60,18 @@ const reducer = handleActions({
 			...action.payload
 		};
 	},
+	[actions.userRegister]: (state, action) => ({ ...state, ...action.payload, isLoading: true }),
+	[actions.userRegisterFail]: (state, action) => ({ ...state, ...action.payload, isLoading: false }),
+	[actions.userRegisterSuccess]: (state, action) => ({ ...state, ...action.payload, isLoading: false }),
+	[actions.userOtp]: (state, action) => ({ ...state, ...action.payload, isLoading: true }),
+	[actions.userOtpSuccess]: (state, action) => ({ ...state, ...action.payload, isLoading: false }),
+	[actions.userOtpFail]: (state, action) => ({ ...state, ...action.payload, isLoading: false }),
+	[actions.userOtpValidate]: (state, action) => ({ ...state, ...action.payload, isLoading: true }),
+	[actions.userOtpValidateSuccess]: (state, action) => ({ ...state, ...action.payload, isLoading: false }),
+	[actions.userOtpValidateFail]: (state, action) => ({ ...state, ...action.payload, isLoading: false }),
+	[actions.userGetProfile]: (state, action) => ({ ...state, ...action.payload, isLoading: true }),
+	[actions.userGetProfileFail]: (state, action) => ({ ...state, ...action.payload, isLoading: false }),
+	[actions.userGetProfileSuccess]: (state, action) => ({ ...state, ...action.payload, isLoading: false })
 }, initialState);
 export default {
 	actions,
