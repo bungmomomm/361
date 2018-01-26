fc58ba3e23388b4ba32676b16785176bcb6ca49e
sarwonous <agus.sarwono@gmail.com>
2018-01-26 11:14:51 +0700

    improve auth reducer

--- src/containers/Mobile/Users/Login/index.js
+++ src/containers/Mobile/Users/Login/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { withCookies } from 'react-cookie';
 import { connect } from 'react-redux';
-import { action } from '@/state/v4/User';
-import { getSessionDomain } from '@/utils';
+import { users } from '@/state/v4/User';
 import { Header, Page, Navigation, Button, Input } from '@/components/mobile';
+import Shared from '@/containers/Mobile/Shared';
+import { setUserCookie } from '@/utils';
 
 
 class Login extends Component {
@@ -18,14 +19,10 @@ class Login extends Component {
 		};
 	}
 
-	componentDidMount() {
-		this.props.dispatch(new action.userAnonymous());
-	}
-
 	async onLogin(e) {
 		try {
-			const { token } = await this.props.dispatch(new action.userLogin(this.props.userCookies, 'agus.sarwono@mataharimall.com', 'iniharusnyapassword'));
-			this.setUserCookie(token);
+			const { token } = await this.props.dispatch(new users.userLogin(this.props.userCookies, 'agus.sarwono@mataharimall.com', 'iniharusnyapassword'));
+			setUserCookie(this.props.cookies, token);
 		} catch (error) {
 			console.log(error);
 		}
@@ -33,27 +30,20 @@ class Login extends Component {
 
 	onUserChange(e) {
 		console.log(e);
-		this.props.dispatch(new action.userNameChange(e));
+		this.props.dispatch(new users.userNameChange(e));
 	}
 
-	setUserCookie(token) {
-		const currentDate = new Date();
-		currentDate.setDate(currentDate.getDate() + (2 * 365));
-		this.props.cookies.set('user.exp', Number(token.expires_in), { domain: getSessionDomain(), expires: currentDate });
-		this.props.cookies.set('user.rf.token', token.refresh_token, { domain: getSessionDomain(), expires: currentDate });
-		this.props.cookies.set('user.token', token.token, { domain: getSessionDomain(), expires: currentDate });
-	}
 
 	handlePick(current) {
 		this.setState({ current });
 	}
 
 	render() {
-		const { userprofile } = this.props.users;
-		const userinfo = Object.keys(userprofile).map((id, key) => {
-			const value = userprofile[id];
+		const { userProfile } = this.props.users;
+		const userinfo = Object.keys(userProfile).map((id, key) => {
+			const value = userProfile[id];
 			return (
-				<li key={id}>{value}</li>
+				<li key={id}>{id} : {value}</li>
 			);
 		});
 
@@ -61,7 +51,9 @@ class Login extends Component {
 			<div style={this.props.style}>
 				<Page>
 					Login
-					{userinfo}
+					<ul>
+						{userinfo}
+					</ul>
 					username
 					<Input value={this.props.users.username} onChange={(e) => this.onUserChange(e.target.value)} />
 					<Button color='primary' size='small' loading={this.props.isLoginLoading} onClick={(e) => this.onLogin(e)} >Login</Button>
@@ -88,5 +80,8 @@ const mapStateToProps = (state) => {
 		isLoginLoading: state.users.isLoading
 	};
 };
+const doAfterAnonymous = (props) => {
+	console.log('code here if you need anon token or token');
+};
 
-export default withCookies(connect(mapStateToProps)(Login));
+export default withCookies(connect(mapStateToProps)(Shared(Login, doAfterAnonymous)));
