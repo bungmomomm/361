4e15f273621e39378cef034e04969116d3ba360c
sunfishr <racmad.prayogo@mataharimall.com>
2018-01-30 11:18:29 +0700

    initial hashtags details actions, reducer, render

--- src/containers/Mobile/Details/HashtagsDetails/index.js
+++ src/containers/Mobile/Details/HashtagsDetails/index.js
@@ -1,24 +1,95 @@
 import React, { Component } from 'react';
 import { withCookies } from 'react-cookie';
-import * as data from '@/data/example/Hashtags';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Header, Page, Navigation, Svg, Grid, Card } from '@/components/mobile';
+import { actions as hdActions } from '@/state/v4/HashtagsDetails';
 
 class HashtagsDetails extends Component {
 	constructor(props) {
 		super(props);
 		this.props = props;
+
+		this.userCookies = this.props.cookies.get('user.token');
+		this.userRFCookies = this.props.cookies.get('user.rf.token');
+		this.source = this.props.cookies.get('user.source');
+		this.hashtag_id = 30;
+	}
+
+
+	componentDidMount() {
+		const dataFetch = {
+			token: this.userCookies,
+			hashtag_id: this.hashtag_id
+		};
+
+		this.props.hashtagDetailAction(dataFetch);
 	}
 
 	render() {
+		const $props = this.props;
+
+		const HeaderPage = {
+			left: (
+				<a href={$props.history.goBack}>
+					<Svg src={'ico_arrow-back-left.svg'} />
+				</a>
+			),
+			center: '#MauGayaItuGampang',
+			right: null
+		};
+
+		const $similarproducts = $props.detail.similarproducts && $props.detail.similarproducts.length > 4 ?
+			$props.detail.similarproducts.slice(0, 4) : $props.detail.similarproducts;
+
 		return (
 			<div>
-				<h1>asdad</h1>
+				<Page>
+					{$props.detail.contentdetail && (
+						<Grid split={1}>
+							<div>
+								<img alt='' src={$props.detail.contentdetail.image} />
+							</div>
+							<div>
+								@{$props.detail.contentdetail.user.username}
+								<p>
+									Post date: {$props.detail.contentdetail.user.created_time} <br />
+									{$props.detail.contentdetail.user.caption}
+								</p>
+							</div>
+							<div>
+								<h2>Get The Look</h2>
+								<Grid split={2}>
+									{$similarproducts.length && $similarproducts.map((product, i) =>
+										(
+											<Card.CatalogSmall key={i} />
+										)
+									)}
+								</Grid>
+							</div>
+						</Grid>
+					)}
+
+					{$props.isLoading && <button>&hellip;</button>}
+				</Page>
+
+				<Header.Modal {...HeaderPage} />
+				<Navigation />
 			</div>);
 	}
 }
 
-HashtagsDetails.defaultProps = {
-	HashtagsDetails: data.HashtagsDetails
+const mapStateToProps = (state) => {
+	return {
+		...state.hashtagsdetails
+	};
 };
 
+const mapDispatchToProps = (dispatch) => {
+	return {
+		hashtagDetailAction: (token) => dispatch(hdActions.hashtagDetailAction(token)),
+		loading: (bool) => dispatch(hdActions.isLoading(bool))
+	};
+};
 
-export default withCookies(HashtagsDetails);
+export default withRouter(withCookies(connect(mapStateToProps, mapDispatchToProps)(HashtagsDetails)));
