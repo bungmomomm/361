1fdc816ade5c0c01778638580c98d83ca09e118e
ijalandhika <ijalandhika@gmail.com>
2018-02-05 16:23:32 +0700

    implement init & promo on home page

--- src/containers/Mobile/Shared/index.js
+++ src/containers/Mobile/Shared/index.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
+import to from 'await-to-js';
 import { actions } from '@/state/v4/Shared';
 import { actions as users } from '@/state/v4/User';
+import { actions as initAction } from '@/state/v4/Home';
 import { setUserCookie } from '@/utils';
 
 const sharedAction = (WrappedComponent, doAfterAnonymousCall) => {
 	class SharedAction extends Component {
-		static initApp(props) {
-			props.dispatch(new actions.totalCartAction(props.userCookies));
-	
-			props.dispatch(new actions.totalLovelistAction(props.userCookies));
-
-			if (typeof doAfterAnonymousCall !== 'undefined') {
-				doAfterAnonymousCall.apply(this, [props]);
-			}
-		}
-
+		
 		constructor(props) {
 			super(props);
 			this.props = props;
@@ -25,43 +18,48 @@ const sharedAction = (WrappedComponent, doAfterAnonymousCall) => {
 			this.userRFCookies = this.props.cookies.get('user.rf.token');
 		}
 
-
 		componentDidMount() {
 			if (this.shouldLoginAnonymous()) {
-				this.loginAnonymous();
-			} else {
-				this.getProfile();
-				this.constructor.initApp(this.props);
-			}
-		}
+				return this.loginAnonymous();
+			} 
 
-		
-		setUserCookie(token) {
-			setUserCookie(this.props.cookies, token);
-			this.constructor.initApp(this.props);
-		}
-
-		async getProfile() {
-			try {
-				await this.props.dispatch(new users.userGetProfile());
-			} catch (error) {
-				// @TODO add 
-				console.log('errr', error);
-			}
+			return this.initProcess(this.userRFCookies);
 		}
 
 		shouldLoginAnonymous() {
 			return (_.isEmpty(this.userCookies) || _.isEmpty(this.userRFCookies));
 		}
 
-		async loginAnonymous() {
-			try {
-				const response = await this.props.dispatch(new users.userAnonymous());
-				this.setUserCookie(response.token);
-			} catch (error) {
-				// @TODO add 
-				console.log('errr', error);
+		initApp() {
+			const { shared, dispatch } = this.props;
+			
+			const loveListService = _.chain(shared).get('serviceUrl.lovelist').value() || false;
+			const orderService = _.chain(shared).get('serviceUrl.order').value() || false;
+
+			dispatch(new actions.totalCartAction(this.userCookies, orderService));
+			dispatch(new actions.totalLovelistAction(this.userCookies, loveListService));
+
+			if (typeof doAfterAnonymousCall !== 'undefined') {
+				doAfterAnonymousCall.apply(this, [this.props]);
 			}
+		}
+
+		async loginAnonymous() {
+			const [err, response] = await to(this.props.dispatch(new users.userAnonymous()));
+			if (err) return ('oops something went wrong');
+
+			
+			setUserCookie(this.props.cookies, response.token);
+			return this.initProcess(response.token);
+		}
+
+		async initProcess(token) {
+			const [err, response] = await to(this.props.dispatch(new initAction.initAction(token)));
+			if (err) return ('oops something went wrong');
+
+			this.initApp(this.props);
+
+			return response;
 		}
 
 		render() {
