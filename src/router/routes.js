3a501e0e4017d37d8cf084568d93765c1852ea2b
Rocky Syahputra <rocky.syahputra@mataharimall.com>
2018-02-05 13:33:08 +0700

    Add profile subpage

--- src/router/routes.js
+++ src/router/routes.js
@@ -36,7 +36,11 @@ let ForgotPassword = defRoute;
 let FilterCategory = defRoute;
 let Promo = defRoute;
 let UserProfile = defRoute;
+let UserProfileView = defRoute;
 let UserProfileEdit = defRoute;
+let UserProfileEditOVO = defRoute;
+let UserProfileEditHP = defRoute;
+let UserProfileEditEmail = defRoute;
 
 if (isMobile()) {
 	/**
@@ -73,7 +77,11 @@ if (isMobile()) {
 	// promo
 	Promo = loadable(() => import('@/containers/Mobile/Discovery/Promo'));
 	UserProfile = loadable(() => import('@/containers/Mobile/Users/Profile'));
+	UserProfileView = loadable(() => import('@/containers/Mobile/Users/Profile/view'));
 	UserProfileEdit = loadable(() => import('@/containers/Mobile/Users/Profile/edit'));
+	UserProfileEditOVO = loadable(() => import('@/containers/Mobile/Users/Profile/editOVO'));
+	UserProfileEditHP = loadable(() => import('@/containers/Mobile/Users/Profile/editHP'));
+	UserProfileEditEmail = loadable(() => import('@/containers/Mobile/Users/Profile/editEmail'));
 } else {
 	/**
 	 * Require main desktop styles
@@ -183,8 +191,24 @@ export default [
 		component: UserProfile
 	},
 	{
+		path: '/profileView',
+		component: UserProfileView
+	},
+	{
 		path: '/profileEdit',
 		component: UserProfileEdit
+	},
+	{
+		path: '/profileEditOVO',
+		component: UserProfileEditOVO
+	},
+	{
+		path: '/profileEditHP',
+		component: UserProfileEditHP
+	},
+	{
+		path: '/profileEditEmail',
+		component: UserProfileEditEmail
 	},
 	{
 		path: '/*', // Page not found handling.
