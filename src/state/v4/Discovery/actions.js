6883a9802df79347d793814a32fdd6c9736374ba
sunfishr <racmad.prayogo@mataharimall.com>
2018-02-06 16:36:08 +0700

    - implement do after anonymous init, serviceUrl - mapStateToProps: props needs other state (safer to just ...state)

--- src/state/v4/Discovery/actions.js
+++ src/state/v4/Discovery/actions.js
@@ -1,4 +1,5 @@
 import { request } from '@/utils';
+import _ from 'lodash';
 import { promos } from './reducer';
 import { actions as scrollerActions } from '@/state/v4/Scroller';
 // import { promo } from '@/data/translations';
@@ -7,9 +8,12 @@ const configs = {
 	defaultPage: 30
 };
 
-const promoAction = ({ token, promoType, query = {} }) => (dispatch) => {
-	const url = `${process.env.MICROSERVICES_URL}${promoType}`;
+const promoAction = ({ token, promoType, query = {} }) => (dispatch, getState) => {
 	dispatch(scrollerActions.onScroll({ loading: true }));
+
+	const { shared } = getState();
+	const baseUrl = _.chain(shared).get('serviceUrl.promo.url').value() || process.env.MICROSERVICES_URL;
+	const url = `${baseUrl}/${promoType}`;
 
 	if (!query.page) {
 		query.page = 1;
@@ -27,9 +31,9 @@ const promoAction = ({ token, promoType, query = {} }) => (dispatch) => {
 		promo[promoType] = response.data.data;
 		dispatch(promos({ promo }));
 
-		const nextLink = promo[promoType].links && promo[promoType].links.next ? new URL(promo[promoType].links.next).searchParams : false;
+		const nextLink = promo[promoType].links && promo[promoType].links.next ? new URL(baseUrl + promo[promoType].links.next).searchParams : false;
 		dispatch(scrollerActions.onScroll({
-			nextData: { token, promoType, query: { page: nextLink ? nextLink.get('page') : false } },
+			nextData: { token, promoType, query: { ...query, page: nextLink ? nextLink.get('page') : false } },
 			nextPage: nextLink !== false,
 			loading: false,
 			loader: promoAction
