bda2a063f579ff856cd302c833cdff53197c0b11
sunfishr <racmad.prayogo@mataharimall.com>
2018-02-06 10:55:06 +0700

    implement hoc scroller n mock up

--- src/containers/Mobile/Discovery/Hashtags/index.js
+++ src/containers/Mobile/Discovery/Hashtags/index.js
@@ -1,123 +1,137 @@
 import React, { Component } from 'react';
 import { withCookies } from 'react-cookie';
-// import { connect } from 'react-redux';
-// import { actions as hashtagActions } from '@/state/v4/Hashtag';
-// import { Header, Page, Navigation, Svg, Grid } from '@/components/mobile';
-import { Page, Header, Svg, Button, Image, Grid } from '@/components/mobile';
-
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { actions } from '@/state/v4/Hashtag';
+import { Header, Page, Navigation, Svg, Grid, Button, Image } from '@/components/mobile';
+import { Link, withRouter } from 'react-router-dom';
+import Shared from '@/containers/Mobile/Shared';
+import Scroller from '@/containers/Mobile/Shared/scroller';
 
 class Hashtags extends Component {
+
 	constructor(props) {
 		super(props);
-		this.state = {
-			gridSizeSmall: true
-		};
+		this.props = props;
+
+		this.userCookies = this.props.cookies.get('user.token');
+		this.switchTag = this.switchTag.bind(this);
+		this.switchMode = this.switchMode.bind(this);
+	}
+
+	componentDidMount() {
+		const { dispatch, location } = this.props;
+		dispatch(actions.initHashtags(this.userCookies, location.hash));
+	}
+
+	switchTag(tag) {
+		const switchTag = tag.replace('#', '').toLowerCase();
+		const { dispatch, hashtag } = this.props;
+
+		if (typeof tag !== 'undefined' && hashtag.active.tag !== switchTag) {
+			dispatch(actions.itemsActiveHashtag(tag));
+
+			if (!hashtag.products[switchTag] && !hashtag.isLoading) {
+				const q = actions.getQuery(this.props.hashtag);
+				const dataFetch = {
+					token: this.userCookies,
+					query: q.query
+				};
+				dispatch(actions.itemsFetchData(dataFetch));
+			}
+		}
+	};
+
+	switchMode(e) {
+		e.preventDefault();
+		const { hashtag, dispatch } = this.props;
+		const mode = hashtag.viewMode === 3 ? 1 : 3;
+		dispatch(actions.switchViewMode(mode));
 	}
 
 	renderGridSmall() {
-		console.log(this);    
+		const { hashtag } = this.props;
+		const items = hashtag.products[hashtag.active.node] && hashtag.products[hashtag.active.node].items
+					? hashtag.products[hashtag.active.node].items : [];
+
 		return (
 			<Grid bordered split={3}>
-				<div><Image local src='temp/thumb-1.jpg' /></div>
-				<div><Image local src='temp/thumb-1.jpg' /></div>
-				<div><Image local src='temp/thumb-1.jpg' /></div>
-				<div><Image local src='temp/thumb-1.jpg' /></div>
-				<div><Image local src='temp/thumb-1.jpg' /></div>
-				<div><Image local src='temp/thumb-1.jpg' /></div>
-				<div><Image local src='temp/thumb-1.jpg' /></div>
-				<div><Image local src='temp/thumb-1.jpg' /></div>
-				<div><Image local src='temp/thumb-1.jpg' /></div>
-				<div><Image local src='temp/thumb-1.jpg' /></div>
-				<div><Image local src='temp/thumb-1.jpg' /></div>
-				<div><Image local src='temp/thumb-1.jpg' /></div>
-				<div><Image local src='temp/thumb-1.jpg' /></div>
-				<div><Image local src='temp/thumb-1.jpg' /></div>
-				<div><Image local src='temp/thumb-1.jpg' /></div>
-				<div><Image local src='temp/thumb-1.jpg' /></div>
-				<div><Image local src='temp/thumb-1.jpg' /></div>
-				<div><Image local src='temp/thumb-1.jpg' /></div>
+				{items.map((product, i) => (
+					<div key={i}><Image src={product.image} /></div>
+				))}
 			</Grid>
 		);
 	}
 
 	renderGridLarge() {
-		console.log(this);
+		const { hashtag } = this.props;
+		const items = hashtag.products[hashtag.active.node] && hashtag.products[hashtag.active.node].items
+					? hashtag.products[hashtag.active.node].items : [];
+
 		return (
 			<div>
-				<div>
-					<Image local src='temp/product-1.jpg' width='100%' />
-					<div className='flex-row padding--medium margin--medium'>
-						<div><Image avatar height={40} width={40} local src='temp/pp.jpg' /></div>
-						<div className='padding--medium'>
-							<div><Link className='font-color--primary' to='/'>@vinensiuswibowo</Link></div>
-							<div><em className='font-small font--lato-normal font-color--grey'>Post date: 13/12/2017</em></div>
+				{items.map((product, i) => (
+					<div key={i}>
+						<Image src={product.image} width='100%' />
+						<div className='flex-row padding--medium margin--medium'>
+							<div><Image avatar height={40} width={40} local src='temp/pp.jpg' /></div>
+							<div className='padding--medium'>
+								<div><Link className='font-color--primary' to='/'>@vinensiuswibowo</Link></div>
+								<div><em className='font-small font--lato-normal font-color--grey'>Post date: 13/12/2017</em></div>
+							</div>
 						</div>
 					</div>
-				</div>
-				<div>
-					<Image local src='temp/product-1.jpg' width='100%' />
-					<div className='flex-row padding--medium margin--medium'>
-						<div><Image avatar height={40} width={40} local src='temp/pp.jpg' /></div>
-						<div className='padding--medium'>
-							<div><Link className='font-color--primary' to='/'>@vinensiuswibowo</Link></div>
-							<div><em className='font-small font--lato-normal font-color--grey'>Post date: 13/12/2017</em></div>
-						</div>
-					</div>
-				</div>
-				<div>
-					<Image local src='temp/product-1.jpg' width='100%' />
-					<div className='flex-row padding--medium margin--medium'>
-						<div><Image avatar height={40} width={40} local src='temp/pp.jpg' /></div>
-						<div className='padding--medium'>
-							<div><Link className='font-color--primary' to='/'>@vinensiuswibowo</Link></div>
-							<div><em className='font-small font--lato-normal font-color--grey'>Post date: 13/12/2017</em></div>
-						</div>
-					</div>
-				</div>
+				))}
 			</div>
 		);
 	}
 
-
 	render() {
+		const { hashtag, history, scroller } = this.props;
+		const tags = hashtag.tags.length > 3 ? hashtag.tags.slice(0, 3) : hashtag.tags;
+
 		const HeaderPage = {
 			left: (
-				<Link to='/brandcategory'>
+				<button onClick={history.goBack}>
 					<Svg src={'ico_arrow-back-left.svg'} />
-				</Link>
+				</button>
 			),
 			center: '#MauGayaItuGampang',
 			right: (
-				<Button onClick={() => this.setState({ gridSizeSmall: !this.state.gridSizeSmall })}>
-					<Svg src={this.state.gridSizeSmall ? 'ico_three-line.svg' : 'ico_grid.svg'} />
+				<Button onClick={this.switchMode}>
+					<Svg src={hashtag.viewMode === 3 ? 'ico_three-line.svg' : 'ico_grid.svg'} />
 				</Button>
 			)
 		};
 
 		return (
-			<div style={this.props.style}>
+			<div>
 				<Page>
 					<div className='margin--medium text-center padding--large'>
 						Upload gaya OOTD kamu di Instagram dengan hashtag #MauGayaItuGampang dan menangin kesempatan tampil di MatahariMall.com!
 					</div>
 					<div className='flex-row flex-center flex-spaceBetween margin--medium padding--large'>
-						<Link to='/'>All</Link>
-						<Link to='/'>#MDSMens</Link>
-						<Link to='/'>#MDSWomens</Link>
-						<Link to='/'>#MDSKids</Link>
+						<Link to={'/hashtags#All'} onClick={() => this.switchTag('#All')}>All</Link>
+						{tags.map((tag, i) => (
+							<Link to={`/hashtags${tag.hashtag}`} onClick={() => this.switchTag(tag.hashtag)} key={i}>{tag.hashtag}</Link>
+						))}
 					</div>
-					{
-						this.state.gridSizeSmall ? this.renderGridSmall() : this.renderGridLarge()
-					}
-					<div className='flex-center padding--medium margin--large'>
-						<Button color='secondary' size='medium' style={{ width: '200px' }} outline>LOAD MORE</Button>
-					</div>
+
+					{hashtag.viewMode === 3 ? this.renderGridSmall() : this.renderGridLarge()}
+
+					{scroller.loading && <button>&hellip;</button>}
 				</Page>
+
 				<Header.Modal {...HeaderPage} />
-			</div>
-		);
+				<Navigation />
+			</div>);
 	}
 }
 
-export default withCookies(Hashtags);
+const mapStateToProps = (state) => {
+	// console.log(state.hashtag);
+	return {
+		...state
+	};
+};
+
+export default withRouter(withCookies(connect(mapStateToProps)(Shared(Scroller(Hashtags)))));
