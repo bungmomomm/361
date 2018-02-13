5194c130e7b327fe1c6097ff828ed1ea0b912d97
Faris Rayhan <frayhan32@gmail.com>
2018-02-13 14:27:44 +0700

    feature/forever-banner
    Enable close button.

--- src/containers/Mobile/Shared/foreverBanner.js
+++ src/containers/Mobile/Shared/foreverBanner.js
@@ -4,12 +4,15 @@ const foreverBanner = (params) => {
 	const inlineStyle = {
 		color: params.text_color
 	};
+	
+	const disableCloseFilter = params.close_button.fg_show === '1' === false;
+	
 	return (
 		<Notification
 			color={params.text.background_color}
 			show={params.show}
 			onClose={params.onClose}
-			disableClose={params.close_button.fg_show}
+			disableClose={disableCloseFilter}
 		>
 			<div style={inlineStyle}>{params.text.text1}</div>
 			<p style={inlineStyle}>{params.text.text2}</p>
