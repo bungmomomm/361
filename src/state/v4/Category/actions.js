c5613a20ba857eb4c25a97a3b7b2e93cdc9b524a
sinoridha <sinoridha@gmail.com>
2018-02-14 16:29:14 +0700

    RVMS-77 - Refactor action Category Menu, use async

--- src/state/v4/Category/actions.js
+++ src/state/v4/Category/actions.js
@@ -1,40 +1,75 @@
 import { request } from '@/utils';
 import { getCategoryMenu, getCategoryBrand, categoryLoading, brandsLoading } from './reducer';
+import _ from 'lodash';
+import to from 'await-to-js';
+import { Promise } from 'es6-promise';
 
-const getCategoryMenuAction = (token, activeSegment) => (dispatch) => {
+const getCategoryMenuAction = (userToken, activeSegment) => async (dispatch, getState) => {
 	dispatch(categoryLoading({ loading: true }));
-	const url = `${process.env.MICROSERVICES_URL}categories/list`;
-	return request({
-		token,
-		path: url,
-		method: 'GET',
-		fullpath: true,
-		query: {
-			segment_id: activeSegment.id
-		}
-	}).then(response => {
+
+	const { shared } = getState();
+	let baseUrl = _.chain(shared).get('serviceUrl.lovelist.url').value() || false;
+
+	// TODO: need to enable baseUrl checking while api ready
+	if (true) baseUrl = process.env.MICROSERVICES_URL;
+	// if (!baseUrl) baseUrl = process.env.MICROSERVICES_URL;
+
+	const [err, response] = await to(
+		request({
+			token: userToken,
+			path: `${baseUrl}categories/list`,
+			method: 'GET',
+			fullpath: true,
+			query: {
+				segment_id: activeSegment.id
+			}
+		})
+	);
+
+	if (response) {
 		const categories = response.data.data;
 		dispatch(getCategoryMenu({ categories, activeSegment }));
 		dispatch(categoryLoading({ loading: false }));
-	});
+
+		return Promise.resolve(response);
+	}
+
+	if (err) return Promise.reject(err);
+
+	return false;
 };
 
-const getBrandsByCategoryIdAction = (token, categoryId) => (dispatch) => {
+const getBrandsByCategoryIdAction = (token, categoryId) => async (dispatch, getState) => {
 	dispatch(brandsLoading({ loadingBrands: true }));
-	const url = `${process.env.MICROSERVICES_URL}featured_brand`;
-	return request({
-		token,
-		path: url,
-		method: 'GET',
-		fullpath: true,
-		query: {
-			category_id: categoryId
-		}
-	}).then(response => {
+
+	const { shared } = getState();
+	let baseUrl = _.chain(shared).get('serviceUrl.lovelist.url').value() || false;
+
+	// TODO: need to enable baseUrl checking while api ready
+	if (true) baseUrl = process.env.MICROSERVICES_URL;
+	// if (!baseUrl) baseUrl = process.env.MICROSERVICES_URL;
+
+	// const url = `${process.env.MICROSERVICES_URL}featured_brand`;
+	const [err, response] = await to(
+		request({
+			token,
+			path: `${baseUrl}featured_brand`,
+			method: 'GET',
+			fullpath: true,
+			query: {
+				category_id: categoryId
+			}
+		})
+	);
+	if (response) {
 		const brands = response.data.data;
 		dispatch(getCategoryBrand({ brands }));
 		dispatch(brandsLoading({ loadingBrands: false }));
-	});
+	};
+
+	if (err) return Promise.reject(err);
+
+	return false;
 };
 
 export default {
