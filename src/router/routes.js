32ec81f7a24ab6245daeaf9be1163079d0fdf64b
sinoridha <sinoridha@gmail.com>
2018-03-14 10:38:13 +0700

    SubCategory - Fix router for feature brand

--- src/router/routes.js
+++ src/router/routes.js
@@ -55,10 +55,9 @@ export default {
 		{
 			path: '/p-:categoryId([0-9]+)/:categoryTitle([a-zA-Z0-9-]+)',
 			component: loadable(() => import('@/containers/Mobile/Discovery/Category/Product'), defaultOptions),
-			exact: true
 		},
 		{
-			path: '/p-:categoryId:([0-9]+)}/:categoryTitle([a-zA-Z0-9-]+):brandTitle(/[a-zA-Z0-9-]+)?',
+			path: '/p-:categoryId:([0-9]+)}/:categoryTitle([a-zA-Z0-9-]+)/:brandTitle([a-zA-Z0-9-]+)',
 			component: loadable(() => import('@/containers/Mobile/Discovery/Category/Product'), defaultOptions)
 		},
 		{
