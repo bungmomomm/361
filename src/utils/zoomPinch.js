35b56b79710d9a4bbb8752a57aeab16eebb1c9c3
sinoridha <sinoridha@gmail.com>
2018-03-26 14:45:05 +0700

    Fix wrong update viewport content

--- src/utils/zoomPinch.js
+++ src/utils/zoomPinch.js
@@ -3,10 +3,10 @@ const enableZoomPinch = (togle = true) => {
 	let content = elViewport.content;
 	if (togle === true) {
 		content = content.replace('user-scalable=no', 'user-scalable=yes');
-		content = content.replace('initial-scale=1', 'initial-scale=2');
+		content = content.replace('maximum-scale=1', 'maximum-scale=2');
 	} else {
 		content = content.replace('user-scalable=yes', 'user-scalable=no');
-		content = content.replace('initial-scale=2', 'initial-scale=1');
+		content = content.replace('maximum-scale=2', 'maximum-scale=1');
 	}
 	elViewport.setAttribute('content', content);
 	return togle;
