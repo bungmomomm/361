87f3b2c01d4aed45b636ee9902c12f496c634c74
ijalandhika <ijalandhika@gmail.com>
2018-04-13 14:54:19 +0700

    content footer & canonical should to desktop url

--- src/components/mobile/SEO/index.js
+++ src/components/mobile/SEO/index.js
@@ -62,7 +62,8 @@ const SEO = ({
 	const seoRobots = variableCheck(bots) ? bots : defaultContent.robots;
 	const seoTitle = variableCheck(titz) ? `${titz} | Mataharimall.com` : defaultContent.title;
 	const seoAlternate = variableCheck(paramAlternate) ? paramAlternate : defaultContent.alternate;
-	const seoCanonial = variableCheck(cano) ? cano : (variableCheck(paramCanonical) ? paramCanonical : window.location.origin + window.location.pathname);
+	const defaultCanonical = `${window.location.protocol}//www.mataharimall.com${window.location.pathname}`;
+	const seoCanonial = variableCheck(cano) ? cano : defaultCanonical;
 
 
 	return (
