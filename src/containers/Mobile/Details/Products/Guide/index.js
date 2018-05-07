3f1e599e8e983f971c564331c56cf421464fc260
Faris Rayhan <frayhan32@gmail.com>
2018-05-07 10:58:38 +0700

    feature/BUMN-161
    Fix the condition when it is coming from MDS then show the table
     otherwise just show the plain text

--- src/containers/Mobile/Details/Products/Guide/index.js
+++ src/containers/Mobile/Details/Products/Guide/index.js
@@ -65,8 +65,6 @@ class Guide extends Component {
 		
 		let showProductSizeGuideDetail = false;
 		
-		const { product } = this.props;
-		
 		const query = queryString.parse(this.props.location.search);
 		
 		if (query.referrer === 'mds') {
@@ -77,24 +75,27 @@ class Guide extends Component {
 			<div>
 				<Page color='white' style={{ overflow: 'auto' }}>
 					<div className='margin--medium-v padding--medium-h'>
-						<Table>
-							<thead>
-								<tr>
-									<th>International</th>
-									<th>Bust(cm)</th>
-									<th>Waist(cm)</th>
-									<th>Hip(cm)</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr><td>XS</td><td>86-92</td><td>69-74</td><td>88-93</td></tr>
-								<tr><td>S</td><td>92-97</td><td>74-79</td><td>93-98</td></tr>
-								<tr><td>M</td><td>97-102</td><td>79-84</td><td>98-103</td></tr>
-								<tr><td>L</td><td>102-107</td><td>84-89</td><td>103-111</td></tr>
-								<tr><td>XL</td><td>107-112</td><td>89-94</td><td>111-116</td></tr>
-							</tbody>
-						</Table>
-						{ product.detail && this.data && showProductSizeGuideDetail === true && (
+						{/* Only show this table when the referrer comes from MDS */}
+						{ showProductSizeGuideDetail === true && (
+							<Table>
+								<thead>
+									<tr>
+										<th>International</th>
+										<th>Bust(cm)</th>
+										<th>Waist(cm)</th>
+										<th>Hip(cm)</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr><td>XS</td><td>86-92</td><td>69-74</td><td>88-93</td></tr>
+									<tr><td>S</td><td>92-97</td><td>74-79</td><td>93-98</td></tr>
+									<tr><td>M</td><td>97-102</td><td>79-84</td><td>98-103</td></tr>
+									<tr><td>L</td><td>102-107</td><td>84-89</td><td>103-111</td></tr>
+									<tr><td>XL</td><td>107-112</td><td>89-94</td><td>111-116</td></tr>
+								</tbody>
+							</Table>
+						)}
+						{ showProductSizeGuideDetail === false && (
 							<div dangerouslySetInnerHTML={{ __html: this.data[0].value }} />
 						)}
 						{/* {!(/^/.test(spec)) && spec } */}
