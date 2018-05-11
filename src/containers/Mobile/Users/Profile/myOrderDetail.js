e9adfefd7a8e7d3cc1ed7979942b457319490da3
Abednego Setyawan <abednego.setyawan@gmail.com>
2018-05-11 14:08:22 +0700

    Fix missing 'Size' information in Order Detail History

--- src/containers/Mobile/Users/Profile/myOrderDetail.js
+++ src/containers/Mobile/Users/Profile/myOrderDetail.js
@@ -134,6 +134,9 @@ class MyOrderDetail extends Component {
 								<div className='flex-row flex-spaceBetween flex-middle margin--medium-t'>
 									<div className='flex-row'>
 										<div>
+											{(item.variant.title && item.variant.value) &&
+												<span className='font-small'>{item.variant.title} <strong>{item.variant.value}</strong></span>
+											}
 											<span className='font-small'>Jumlah <strong>{item.qty}</strong></span>
 											<strong className='margin--small-t'>{item.pricing.formatted.price}</strong>
 										</div>
