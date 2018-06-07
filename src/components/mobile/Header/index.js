5e445b13da8ab8ef3b872fecb689cb87b5cd28e7
Faris Rayhan <frayhan32@gmail.com>
2018-06-07 13:42:53 +0700

    Convert stateless header to statefull component

--- src/components/mobile/Header/index.js
+++ src/components/mobile/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Input from '../Input';
 import styles from './header.scss';
 import Svg from '../Svg';
@@ -9,110 +9,142 @@ import ShoppingBagItem from './shoppingBagItem';
 import Modal from './modal';
 import { withRouter, Link } from 'react-router-dom';
 
-const Header = props => {
-
-	// const { location, lovelist } = props;
-
-	let searchReference = 'home';
-
-	if (location.pathname === '/category') {
-		searchReference = 'category';
+class Header extends Component {
+	
+	constructor(props) {
+		super(props);
+		this.props = props;
+		this.state = {
+			showCart: false,
+			showUserMenu: false
+		};
+		
+		this.toggleCart = this.toggleCart.bind(this);
+		
 	}
 	
-	console.log(searchReference);
+	toggleCart() {
+		this.setState((prevState) => {
+			let value = true;
+			if (prevState.showCart === true) {
+				value = false;
+			}
+			return {
+				showCart: value
+			};
+		});
+	}
 	
-	return (
-		<div>
-			<nav className={styles.container}>
-				<div className={styles.wrapper}>
-					<div className={styles.header}>
-						<div className={styles.topbar}>
-							<div className='container'>
-								<div className='row'>
-									<div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'>
-										<Link className='d-flex flex-middle' to='https://matahari.com'><Svg src='ico_matahari.svg' /> Matahari Department Store</Link>
-									</div>
-									<div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'>
-										<Link className='d-flex flex-middle' to='https://mataharimall.com'><Svg src='ico_mataharimall.svg' /> MatahariMall.com</Link>
-									</div>
-									<div className='col-md-offset-6 col-lg-offset-6 col-xs-2 col-sm-2 col-md-2 col-lg-2 text-right'>
-										<Link to='https://mataharimall.com'>Tentang 361</Link>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className='container'>
-							<div className='row padding--medium-v flex-middle'>
-								<div className='col-xs-2 col-sm-2 col-md-3 col-lg-3'>
-									<Link className='d-flex flex-middle' to='/mau-gaya-itu-gampang'><Svg src='logo.svg' /></Link>
-								</div>
-								<div className='col-xs-2 col-md-6 col-lg-6'>
-									<div className={styles.center}>
-										<Input
-											onFocus={() => props.history.push(`/search?ref=${searchReference}`)}
-											placeholder=''
-											value=''
-										/>
-										<div className={styles.dummyplaceholder}>
-											<span className='padding--small-h'>Cari produk</span>
-											<span><Svg src='ico_search_361_small.svg' /></span>
+	
+	render() {
+		// const { location, lovelist } = props;
+		
+		let searchReference = 'home';
+		
+		if (location.pathname === '/category') {
+			searchReference = 'category';
+		}
+		
+		console.log(searchReference);
+		
+		return (
+			<div>
+				<nav className={styles.container}>
+					<div className={styles.wrapper}>
+						<div className={styles.header}>
+							<div className={styles.topbar}>
+								<div className='container'>
+									<div className='row'>
+										<div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'>
+											<Link className='d-flex flex-middle' to='https://matahari.com'><Svg src='ico_matahari.svg' /> Matahari Department Store</Link>
+										</div>
+										<div className='col-xs-2 col-sm-2 col-md-2 col-lg-2'>
+											<Link className='d-flex flex-middle' to='https://mataharimall.com'><Svg src='ico_mataharimall.svg' /> MatahariMall.com</Link>
+										</div>
+										<div className='col-md-offset-6 col-lg-offset-6 col-xs-2 col-sm-2 col-md-2 col-lg-2 text-right'>
+											<Link to='https://mataharimall.com'>Tentang 361</Link>
 										</div>
 									</div>
 								</div>
-								<div className='col-xs-2 col-md-1 col-lg-1'>
-									<Link className='d-flex flex-middle flex-center' to=''><Svg src='ico_hashtags.svg' /></Link>
-								</div>
-								<div className='col-xs-2 col-md-1 col-lg-1 relative' >
-									<Svg src='ico_user.svg' />
-								</div>
-								<div className='col-xs-2 col-md-1 col-lg-1 d-flex flex-middle flex-center relative'>
-									<Svg src='ico_bag_361.svg' />
-									<ShoppingBagItem
-										show
-										data={[
-											{
-												name: 'MALE CASUAL SHOES',
-												size: '36',
-												price: 240000,
-												total: 1,
-												image: 'https://picsum.photos/200/300?image=0'
-											},
-											{
-												name: 'FEMALE CASUAL SHOES',
-												size: '37',
-												price: 250000,
-												total: 2,
-												image: 'https://picsum.photos/200/300?image=0'
-											}
-										]}
-									/>
-								</div>
 							</div>
-						</div>
-						<div className={styles.main}>
 							<div className='container'>
-								<ul>
-									<li><a className='flex-middle d-flex'>Sepatu Wanita <Svg className='margin--small-l' src='ico_arrow-down.svg' /></a></li>
-									<li><a className='flex-middle d-flex'>Sepatu Pria <Svg className='margin--small-l' src='ico_arrow-down.svg' /></a></li>
-									<li><a className='flex-middle d-flex'>Pakaian <Svg className='margin--small-l' src='ico_arrow-down.svg' /></a></li>
-									<li><a className='flex-middle d-flex'>Aksesoris <Svg className='margin--small-l' src='ico_arrow-down.svg' /></a></li>
-									<li className={styles.sale}><a>Sale</a></li>
-								</ul>
+								<div className='row padding--medium-v flex-middle'>
+									<div className='col-xs-2 col-sm-2 col-md-3 col-lg-3'>
+										<Link className='d-flex flex-middle' to='/mau-gaya-itu-gampang'><Svg src='logo.svg' /></Link>
+									</div>
+									<div className='col-xs-2 col-md-6 col-lg-6'>
+										<div className={styles.center}>
+											<Input
+												onFocus={() => this.props.history.push(`/search?ref=${searchReference}`)}
+												placeholder=''
+												value=''
+											/>
+											<div className={styles.dummyplaceholder}>
+												<span className='padding--small-h'>Cari produk</span>
+												<span><Svg src='ico_search_361_small.svg' /></span>
+											</div>
+										</div>
+									</div>
+									<div className='col-xs-2 col-md-1 col-lg-1'>
+										<Link className='d-flex flex-middle flex-center' to=''><Svg src='ico_hashtags.svg' /></Link>
+									</div>
+									<div className='col-xs-2 col-md-1 col-lg-1 relative' >
+										<Svg src='ico_user.svg' />
+									</div>
+									<div className='col-xs-2 col-md-1 col-lg-1 d-flex flex-middle flex-center relative'>
+										<Svg
+											src='ico_bag_361.svg'
+											onClick={() => {
+												this.toggleCart();
+											}}
+										/>
+										<ShoppingBagItem
+											show={this.state.showCart}
+											data={[
+												{
+													name: 'MALE CASUAL SHOES',
+													size: '36',
+													price: 240000,
+													total: 1,
+													image: 'https://picsum.photos/200/300?image=0'
+												},
+												{
+													name: 'FEMALE CASUAL SHOES',
+													size: '37',
+													price: 250000,
+													total: 2,
+													image: 'https://picsum.photos/200/300?image=0'
+												}
+											]}
+										/>
+									</div>
+								</div>
+							</div>
+							<div className={styles.main}>
+								<div className='container'>
+									<ul>
+										<li><a className='flex-middle d-flex'>Sepatu Wanita <Svg className='margin--small-l' src='ico_arrow-down.svg' /></a></li>
+										<li><a className='flex-middle d-flex'>Sepatu Pria <Svg className='margin--small-l' src='ico_arrow-down.svg' /></a></li>
+										<li><a className='flex-middle d-flex'>Pakaian <Svg className='margin--small-l' src='ico_arrow-down.svg' /></a></li>
+										<li><a className='flex-middle d-flex'>Aksesoris <Svg className='margin--small-l' src='ico_arrow-down.svg' /></a></li>
+										<li className={styles.sale}><a>Sale</a></li>
+									</ul>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				{
-					props.rows &&
-					<div className={styles.subHeaderWrapper}>
-						<div className={styles.subHeader}>
-							{props.rows}
+					{
+						this.props.rows &&
+						<div className={styles.subHeaderWrapper}>
+							<div className={styles.subHeader}>
+								{this.props.rows}
+							</div>
 						</div>
-					</div>
-				}
-			</nav>
-		</div>
-	);
+					}
+				</nav>
+			</div>
+		);
+	}
 };
 
 Header.Search = Search;
